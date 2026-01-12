51f05099e463cbcafa2fb452d615182ac2203a9a
Izzul <100136488+Noturbae21@users.noreply.github.com>
2026-01-12 21:37:31 +0700

    Ngasih colour pelet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Kita namain 'theme' biar gampang panggilnya
+        'theme-bg': '#F1F3E0',    // Warna krem (buat background utama)
+        'theme-soft': '#D2DCB6',  // Hijau muda pudar (buat card/section selingan)
+        'theme-main': '#A1BC98',  // Hijau sage utama (buat tombol/navbar)
+        'theme-dark': '#778873',  // Hijau tua (buat teks/judul/footer)
+      },
     },
-
-    plugins: [forms],
-};
+  },
+  plugins: [],
+}
