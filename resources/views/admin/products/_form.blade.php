8d3426899e7f03dfde6ab176ec32ef04e2ffab10
Izzul <100136488+Noturbae21@users.noreply.github.com>
2026-01-14 08:59:26 +0700

    adjustmen di bagian manage produk admin yang upload gambar

--- resources/views/admin/products/_form.blade.php
+++ resources/views/admin/products/_form.blade.php
@@ -66,7 +66,7 @@
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                     </div>
                     <p class="text-gray-500 text-sm">Drag & Drop or <span class="text-theme-main font-bold">Click</span> to Upload</p>
-                    <p class="text-xs text-gray-400">JPG, PNG up to 2MB</p>
+                    <p class="text-xs text-gray-400">JPG, PNG</p>
                 </div>
             @endif
         </div>
